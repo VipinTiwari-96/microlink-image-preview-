e8b8a81c6bdc4f44e57979a1c88934a7f66b2b4b
vipin tiwari <vipin.tiwari@devslane.com>
2024-08-02 15:33:29 +0530

    move to yarn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { ScreenShotModel } from "./models";
 import LinkPreview from "./components/LinkPreview";
-import Microlink from "@microlink/react";
 
 const dummyUrls: ScreenShotModel[] = [
   {
